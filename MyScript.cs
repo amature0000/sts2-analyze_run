58821f7f38628e04f29481c2e5a3736afe1ed61a
Ju hyunsoo <155796133+amature0000@users.noreply.github.com>
2026-04-22 20:21:59 +0900

    Add files via upload

--- MyScript.cs
+++ MyScript.cs
@@ -5,6 +5,8 @@ using MegaCrit.Sts2.Core.Nodes.Screens.RunHistoryScreen;
 using MegaCrit.Sts2.Core.Runs;
 using System;
 using System.Text.Json;
+using System.Diagnostics;
+using System.IO;
 
 [ModInitializer("ModInit")]
 public static class ModStart
@@ -20,24 +22,45 @@ public static class ModStart
 [HarmonyPatch(typeof(NRunHistory), "DisplayRun")]
 public static class RunHistoryPostfix
 {
+    private static readonly string ModFolderPath;
+    private static readonly string HtmlPath;
+    private static readonly string DataJsPath;
+    private static bool _browserOpened = false;
+
+    static RunHistoryPostfix()
+    {
+        string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+        string rootDir = Directory.GetParent(baseDir).Parent.FullName;
+        
+        ModFolderPath = Path.Combine(rootDir, "mods", "sharerun");
+        HtmlPath = Path.Combine(ModFolderPath, "index.html");
+        DataJsPath = Path.Combine(ModFolderPath, "sharerun_history.js");
+    }
+
     public static void Postfix(object __instance, RunHistory history)
     {
-        GD.Print("[sharerun]run history displayed");
-        if (history.MapPointHistory == null) return;
-
-        var options = new JsonSerializerOptions{WriteIndented = true};
-        GD.Print("[sharerun] starting process");
         try
         {
-            string jsonString = JsonSerializer.Serialize(history, options);
-            string jsString = $"window.runData = {jsonString};";
-            System.IO.File.WriteAllText("sharerun_history.js", jsonString);
-            GD.Print("[sharerun] dumped run history");
+            if (history.MapPointHistory == null) return;
+
+            GD.Print("[sharerun] starting process");
+            string jsonString = JsonSerializer.Serialize(history);
+            string jsString = $"rundata = {jsonString};";
+            System.IO.File.WriteAllText(DataJsPath, jsString);
+            GD.Print("[sharerun] ==> dumped run history");
+
+            GD.Print("[sharerun] ==> opening browser");
+            if (!_browserOpened)
+            {
+                Process.Start(new ProcessStartInfo(HtmlPath){ UseShellExecute = true });
+                _browserOpened = true;
+                GD.Print("[sharerun] == ==> opened browser");
+            }
         }
         catch (Exception e)
         {
             GD.PrintErr($"[sharerun] failed to dump: {e.Message}");
         }
-        GD.Print("[sharerun] ended process");
+        GD.Print("[sharerun] end process");
     }
 }
